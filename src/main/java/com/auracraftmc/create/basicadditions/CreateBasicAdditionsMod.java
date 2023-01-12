919e6572268ff500cc8ddb05404fbbc9ad76a70c
RedstoneGamer123 <49125888+RedstoneGamer123@users.noreply.github.com>
2023-01-11 23:31:23 -0500

    Fixed Missing Recipes

--- src/main/java/com/auracraftmc/create/basicadditions/CreateBasicAdditionsMod.java
+++ src/main/java/com/auracraftmc/create/basicadditions/CreateBasicAdditionsMod.java
@@ -1,16 +1,18 @@
 package com.auracraftmc.create.basicadditions;
 
-import com.auracraftmc.create.basicadditions.registries.Blocks;
-import com.auracraftmc.create.basicadditions.registries.Items;
-import com.auracraftmc.create.basicadditions.registries.TileEntities;
-import com.auracraftmc.create.basicadditions.tabs.CreateBasicAdditionsTab;
+import com.auracraftmc.create.basicadditions.recipes.*;
+import com.auracraftmc.create.basicadditions.registries.*;
+import com.auracraftmc.create.basicadditions.tabs.*;
 import com.mojang.logging.LogUtils;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 
-import io.netty.handler.codec.http2.Http2FrameReader.Configuration;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 import org.slf4j.Logger;
 
@@ -21,23 +23,33 @@ public class CreateBasicAdditionsMod {
 	public static final String NAME = "Create: Basic Additions";
 	public static final String VERSION = "1.0.0";
 
-	//public static final ModInfo MODINFO = new ModInfo(MODID, NAME, VERSION);
-
 	public static CreateBasicAdditionsMod instance;
 	public static final Logger logger = LogUtils.getLogger();
-	public static Configuration config;
-
-	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MODID);
 	
 	public static final CreativeModeTab MAIN_TAB = new CreateBasicAdditionsTab();
+
+	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MODID).creativeModeTab(() -> MAIN_TAB, NAME);
+	
 	
 	public CreateBasicAdditionsMod() {
 		instance = this;
+		
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		REGISTRATE.registerEventListeners(FMLJavaModLoadingContext.get().getModEventBus());
+		REGISTRATE.registerEventListeners(modEventBus);
 		
 		Blocks.load();
 		Items.load();
 		TileEntities.load();
+		
+		modEventBus.addListener(EventPriority.LOWEST, this::gatherData);
+	}
+
+	public void gatherData(GatherDataEvent event) {
+		DataGenerator gen = event.getGenerator();
+
+		if(event.includeServer()) {
+			gen.addProvider(new StandardRecipeProvider(gen));
+		}
 	}
 }
---
+++ src/main/java/com/auracraftmc/create/basicadditions/recipes/StandardRecipeProvider.java
@@ -0,0 +1,79 @@
+package com.auracraftmc.create.basicadditions.recipes;
+
+import java.util.function.Consumer;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.auracraftmc.create.basicadditions.CreateBasicAdditionsMod;
+import com.auracraftmc.create.basicadditions.registries.Blocks;
+import com.auracraftmc.create.basicadditions.registries.Items;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.foundation.utility.RegisteredObjects;
+
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ItemLike;
+
+public class StandardRecipeProvider extends RecipeProvider {
+
+	public StandardRecipeProvider(DataGenerator gen) {
+		super(gen);
+	}
+	
+    @Override
+    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+    	generateShaped(consumer);
+    	generateShapeless(consumer);
+    	generateConversions(consumer);
+    }
+    
+    private void generateShaped(Consumer<FinishedRecipe> consumer) {
+    	ShapedRecipeBuilder.shaped(Blocks.BRASS_GEARBOX.get())
+    			.unlockedBy("has_item", inventoryTrigger(ItemPredicate.Builder.item().of(AllBlocks.COGWHEEL.get()).build()))
+    			.pattern(" c ")
+    			.pattern("cbc")
+    			.pattern(" c ")
+    			.define('c', AllBlocks.COGWHEEL.get())
+    			.define('b', AllBlocks.BRASS_CASING.get())
+    			.save(consumer);
+    }
+    
+    private void generateShapeless(Consumer<FinishedRecipe> consumer) {
+    	ShapelessRecipeBuilder.shapeless(Blocks.BASIC_GEARSHIFT.get())
+				.unlockedBy("has_item", inventoryTrigger(ItemPredicate.Builder.item().of(AllBlocks.ANDESITE_CASING.get()).build()))
+    			.requires(AllBlocks.ANDESITE_CASING.get())
+    			.requires(AllBlocks.COGWHEEL.get())
+    			.save(consumer);
+    }
+    
+    private void generateConversions(Consumer<FinishedRecipe> consumer) {
+    	ShapelessRecipeBuilder.shapeless(Blocks.BRASS_GEARBOX.get())
+				.unlockedBy("has_item", inventoryTrigger(ItemPredicate.Builder.item().of(Items.VERTICAL_BRASS_GEARBOX.get()).build()))
+    			.requires(Items.VERTICAL_BRASS_GEARBOX.get())
+    			.save(consumer, recipeId("workbench", Blocks.BRASS_GEARBOX.get(), "conversion"));
+    	
+    	ShapelessRecipeBuilder.shapeless(Items.VERTICAL_BRASS_GEARBOX.get())
+				.unlockedBy("has_item", inventoryTrigger(ItemPredicate.Builder.item().of(Blocks.BRASS_GEARBOX.get()).build()))
+    			.requires(Blocks.BRASS_GEARBOX.get())
+    			.save(consumer, recipeId("workbench", Items.VERTICAL_BRASS_GEARBOX.get(), "conversion"));
+    }
+    
+    private ResourceLocation recipeId(@Nonnull String recipeType, @Nonnull ItemLike result) {
+    	return recipeId(recipeType, result, null);
+    }
+    
+    private ResourceLocation recipeId(@Nonnull String recipeType, @Nonnull ItemLike result, @Nullable String suffix) {
+    	ResourceLocation id = result.asItem().getRegistryName();
+    	
+    	if(id == null) throw new IllegalArgumentException("Could not get key for item " + result.asItem() + "!");
+    	
+    	return new ResourceLocation(CreateBasicAdditionsMod.MODID, recipeType + "/" + id.getPath() + (suffix != null ? "_" + suffix : ""));
+    }
+}
